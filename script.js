d1897107086563ca0f75db17ae2e1296b1f03cb0
Arunava Banerjee <100664086+arunava2018@users.noreply.github.com>
2024-01-11 23:28:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let type = new Typed("#changing-text", {
+  strings: [
+    "Competitive Programmer",
+    "Front-end Developer",
+    "3rd Year Student",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
